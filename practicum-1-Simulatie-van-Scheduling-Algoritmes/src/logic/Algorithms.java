fdae73c83387b951c0b464b2efa73672e71c7789
dieter.deschuiteneer <dieter.deschuiteneer@student.odisee.be>
2018-02-24 19:54:46 +0100

    Added sjf algorithm

--- practicum-1-Simulatie-van-Scheduling-Algoritmes/src/logic/Algorithms.java
+++ practicum-1-Simulatie-van-Scheduling-Algoritmes/src/logic/Algorithms.java
@@ -2,9 +2,10 @@ package logic;
 
 import dataentities.Process;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+import java.util.stream.Collectors;
 
 public class Algorithms {
 
@@ -23,11 +24,44 @@ public class Algorithms {
 
 
     public void executeSJF(List<Process> processList) {
-        int currentTime = 0;
-        Queue processQueue = new LinkedList();
-        for (Process Proce: processList
-             ) {
+        List<Process> completed = new LinkedList<>();
+        List<Process> arrivedProcessesList = new LinkedList<>();
 
+
+        arrivedProcessesList.add(processList.get(0));
+        processList.remove(0);
+        arrivedProcessesList.get(0).setEndTime(arrivedProcessesList.get(0).getArrivalTime() + arrivedProcessesList.get(0).getServiceTime());
+        int time =0;
+        time = arrivedProcessesList.get(0).getArrivalTime();
+
+
+        while(processList.size() != 0 && arrivedProcessesList.size() != 0) {
+
+            //find the new processes
+            int finalTime = time;
+            List<Process> newProcessList = processList.stream().filter(p-> p.getArrivalTime() <= finalTime).collect(Collectors.toList());
+            if(newProcessList.size() == 0 && arrivedProcessesList.size() ==0) {
+                newProcessList.add(processList.get(0));
+                time = processList.get(0).getArrivalTime();
+            }
+
+            arrivedProcessesList.addAll(newProcessList);
+            processList.removeAll(newProcessList);
+
+
+            //find Shortest process
+            Process shortestProcess = arrivedProcessesList.stream().sorted(Comparator.comparing(Process::getServiceTime)).findFirst().get();
+            arrivedProcessesList.remove(shortestProcess);
+
+            //process the shorted process
+            time = time + time+shortestProcess.getServiceTime();
+            shortestProcess.setEndTime(time);
+            completed.add( shortestProcess);
         }
+
+
+        processList = (List)Logic.deepClone(completed);
+
+
     }
 }
